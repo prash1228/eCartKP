7dc4d4e27d90029edd091fd197bd0b3a2a0a3c2a
prash1228 <prashanthswengineer@gmail.com>
2022-10-16 22:32:26 +0530

    initial commit

---
+++ eMall/src/main/java/com/ecartkp/eMall/EMallApplication.java
@@ -0,0 +1,13 @@
+package com.ecartkp.eMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMallApplication.class, args);
+	}
+
+}
---
+++ eMall/src/test/java/com/ecartkp/eMall/EMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecartkp.eMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
